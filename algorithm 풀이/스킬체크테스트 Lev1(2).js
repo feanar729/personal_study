574869d90ff6f93dfbd9ec5ac43cc0dcd1bcdaeb
feanar729 <feanar729@gmail.com>
2019-04-29 11:42:44 +0900

    update: codewars 알고리즘 진행 및 내용 추가(진행중)

--- algorithm 풀이/스킬체크테스트 Lev1(2).js
+++ algorithm 풀이/스킬체크테스트 Lev1(2).js
@@ -1,5 +1,6 @@
 // 프로그래머스 쇠막대기
 // https://programmers.co.kr/learn/courses/30/lessons/42585
+'use-strict';
 
 function solution(arrangement) {
   let answer = 0;
@@ -16,3 +17,45 @@ function solution(arrangement) {
   }
   return answer;
 }
+
+// Code Wars : Directions Reduction (5kyu)
+// https://www.codewars.com/kata/550f22f4d758534c1100025a/train/javascript
+
+const cancelMap = {
+  NORTH: 'SOUTH',
+  SOUTH: 'NORTH',
+  EAST: 'WEST',
+  WEST: 'EAST',
+};
+
+function dirReduc(arr) {
+  let changed = false;
+  arr.forEach((val, index) => {
+    if (val === cancelMap[arr[index]]) {
+      console.log(val, index);
+      // arr.splice(index, 2);
+      // changed = true;
+    }
+  });
+  for (let index in arr) {
+    // console.log(arr[index], cancelMap[arr[index]]);
+    if (arr[index + 1] === cancelMap[arr[index]]) {
+      arr.splice(index, 2);
+      changed = true;
+    }
+  }
+  // if array was altered at all
+  // recurse to check again for new matches
+  return changed ? dirReduc(arr) : arr;
+}
+
+const test = dirReduc([
+  'NORTH',
+  'SOUTH',
+  'SOUTH',
+  'EAST',
+  'WEST',
+  'NORTH',
+  'WEST',
+]);
+console.log(test);
