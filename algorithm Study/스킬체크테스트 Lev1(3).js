67e85d2de30973fcdfa7ebc1f39de311eef6a88d
feanar729 <feanar729@gmail.com>
2019-04-30 19:38:27 +0900

    update: 내용 변경

---
+++ algorithm Study/스킬체크테스트 Lev1(3).js
@@ -0,0 +1,44 @@
+// 프로그래머스 프린터 [미해결]
+// https://programmers.co.kr/learn/courses/30/lessons/42587?language=javascript
+function solution(priorities, location) {
+  let finalArr = [];
+  priorities.map((val, index, arr) => {
+    // console.log(val, index, arr[index + 1]);
+    // if (val > arr[index + 1]) console.log(val);
+  });
+
+  const test = priorities.reduce((acc, val, index, arr) => {
+    return Math.max(acc, val);
+  });
+  finalArr.push(test);
+  // console.log(test, finalArr);
+}
+
+const test = solution([2, 1, 3, 2], 0);
+// console.log(test);
+
+// Code Wars : Find the unique number
+// https://www.codewars.com/kata/585d7d5adb20cf33cb000235/train/javascript
+function findUniq(arr) {
+  // do magic
+  const filtering = arr.filter(val => {
+    return arr.indexOf(val) === arr.lastIndexOf(val);
+  });
+  for (let val of filtering) {
+    return val;
+  }
+}
+
+const test1 = findUniq([0, 1, 0]); // 1
+const test2 = findUniq([1, 1, 1, 2, 1, 1]); // 2
+const test3 = findUniq([3, 10, 3, 3, 3]); // 10
+
+// console.log(test1);
+
+// -------------------------------------------------------------------------------------
+// Best Practice
+// -------------------------------------------------------------------------------------
+
+function findUniq(arr) {
+  return arr.find(n => arr.indexOf(n) === arr.lastIndexOf(n));
+}
