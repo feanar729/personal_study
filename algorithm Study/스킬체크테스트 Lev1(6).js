3cbbc32d42c17403c4d145845c56fd4d51da1d02
feanar729 <feanar729@gmail.com>
2019-05-14 00:29:52 +0900

    add: algorithm 풀이 시도 파일 추가

--- algorithm Study/스킬체크테스트 Lev1(6).js
+++ algorithm Study/스킬체크테스트 Lev1(6).js
@@ -1,4 +1,4 @@
-// Code Wars : Moving Zeros To The End (5 kyu)
+// Code Wars : Moving Zeros To The End (5 kyu) [미해결]
 // https://www.codewars.com/kata/52597aa56021e91c93000cb0/train/javascript
 
 var moveZeros = function(arr) {
