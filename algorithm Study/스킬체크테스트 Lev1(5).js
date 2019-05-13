37cc60de08b895ea3abdb7656ac2476480314ccb
feanar729 <feanar729@gmail.com>
2019-05-13 23:15:57 +0900

    add & update: algorithm 풀이 추가 및 시도 추가

--- algorithm Study/스킬체크테스트 Lev1(5).js
+++ algorithm Study/스킬체크테스트 Lev1(5).js
@@ -8,25 +8,27 @@ function solution(bridge_length, weight, truck_weights) {
 
 // Code Wars : Directions Reduction (5kyu)
 // https://www.codewars.com/kata/550f22f4d758534c1100025a/train/javascript
+// https://repl.it/@megan_fisher/Solution-Directions-Reduction[참고]
 
 function dirReduc(dirs) {
-  let i = 0;
-  while (i < dirs.length) {
-    if (
-      (dirs[i] === 'NORTH' && dirs[i + 1] === 'SOUTH') ||
-      (dirs[i] === 'EAST' && dirs[i + 1] === 'WEST') ||
-      (dirs[i] === 'SOUTH' && dirs[i + 1] === 'NORTH') ||
-      (dirs[i] === 'WEST' && dirs[i + 1] === 'EAST')
-    ) {
-      dirs.splice(i, 2);
-      i--;
-    } else {
-      i++;
-    }
-  }
+  const cancelMap = {
+    NORTH: 'SOUTH',
+    SOUTH: 'NORTH',
+    EAST: 'WEST',
+    WEST: 'EAST',
+  };
 
-  return dirs;
+  const test = dirs.map((val, index) => {
+    if ((val, cancelMap[dirs[index + 1]])) dirs.splice(index, 2);
+    else return val;
+  });
+  console.log(test);
 }
 
-const test = dirReduc(['NORTH', 'SOUTH', 'SOUTH', 'EAST', 'WEST', 'NORTH', 'WEST']);
-console.log(test);
+const test1 = dirReduc(['NORTH', 'SOUTH', 'SOUTH', 'EAST', 'WEST', 'NORTH', 'WEST']);
+// const test2 = dirReduc(['NORTH', 'WEST', 'SOUTH', 'EAST']);
+// const test3 = dirReduc(['NORTH', 'SOUTH', 'EAST', 'WEST', 'EAST', 'WEST']);
+
+// console.log(test1); // ["WEST"]
+// console.log(test2); // ["NORTH", "WEST", "SOUTH", "EAST"]
+// console.log(test3); // []
---
+++ algorithm Study/스킬체크테스트 Lev1(6).js
@@ -0,0 +1,6 @@
+// Code Wars : Moving Zeros To The End (5 kyu)
+// https://www.codewars.com/kata/52597aa56021e91c93000cb0/train/javascript
+
+var moveZeros = function(arr) {
+  // TODO: Program me
+};
