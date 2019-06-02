507ab9581bb72e5e2a51ea2e598fcb4ba4ce0b08
feanar729 <feanar729@gmail.com>
2019-06-02 15:23:08 +0900

    add & update: replica Movie Mini Project copy

---
+++ React Study/nomflix/README.md
@@ -0,0 +1,18 @@
+# Nomflix
+
+Learning React and ES6 by building a Movie Discovery App.
+
+- [ ] home
+- [ ] TV Shows
+- [ ] Search
+- [ ] Detail
+
+## API Verbs
+- [x] Now playing (Movie)
+- [x] Upcoming (Movie)
+- [x] Top Rated (TV, Movie)
+- [x] Popular (TV, X)
+- [x] Airing Today (TV)
+- [x] TV Show Detail
+- [x] Movie Detail
+- [x] Search (Movie, TV)
---
+++ React Study/nomflix/src/Routes/TV/TVContainer.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import TVPresenter from './TVPresenter';
+import { tvApi } from '../../api';
+
+export default class extends React.Component {
+  state = {
+    topRated: null,
+    popular: null,
+    airingToday: null,
+    error: null,
+    loading: true,
+  };
+
+  async componentDidMount() {
+    try {
+      const {
+        data: { results: topRated },
+      } = await tvApi.topRated();
+      const {
+        data: { results: airingToday },
+      } = await tvApi.airingToday();
+      const {
+        data: { results: popular },
+      } = await tvApi.popular();
+
+      this.setState({
+        topRated,
+        airingToday,
+        popular,
+      });
+    } catch {
+      this.setState({
+        error: "Can't find TV information.",
+      });
+    } finally {
+      this.setState({
+        loading: false,
+      });
+    }
+  }
+
+  render() {
+    const { topRated, popular, airingToday, loading, error } = this.state;
+    return (
+      <TVPresenter
+        topRated={topRated}
+        popular={popular}
+        airingToday={airingToday}
+        loading={loading}
+        error={error}
+      />
+    );
+  }
+}
